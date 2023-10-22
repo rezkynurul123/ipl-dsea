6a522edd2468d3c77298c90cc8b650c1e08135fd
rezkynurulf <rezkynurulfadlillah123@gmail.com>
2023-10-23 04:37:52 +0700

    menambah code awal kasus liskov

---
+++ lsp/Smartphone.java
@@ -0,0 +1,14 @@
+package lsp;
+
+import java.util.Date;
+
+public class Smartphone extends Product {
+    @Override
+    String getName() {
+        return "Samsung S10+ Limited Edition";
+    }
+    @Override
+    Date getExpiredDate() {
+    return new Date(); // ???????
+    }
+}
